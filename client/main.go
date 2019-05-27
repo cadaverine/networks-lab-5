55ca7cc14ee708b45064001baefc4e6c1ca523c5
cadaverine <kennville@gmail.com>
2019-05-28 00:42:33 +0300

    feat(client): добавлена реализация клиента

--- client/main.go
+++ client/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	host, port := "localhost", "8080"
+
+	switch len(os.Args) {
+	case 3:
+		port = os.Args[2]
+		host = os.Args[1]
+	case 2:
+		host = os.Args[1]
+	}
+
+	ip := host + ":" + port
+
+	client := http.Client{}
+
+	response, err := client.Get("http://" + ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+	io.Copy(os.Stdout, response.Body)
 }

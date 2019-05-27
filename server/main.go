8afa1c2dcbf6b22934724352a37edef573da711a
cadaverine <kennville@gmail.com>
2019-05-27 23:39:16 +0300

    feat(server): добавлена базовая реализация http-сервера

--- server/main.go
+++ server/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Server response")
+}
+
+func serve(ip string) {
+	log.Fatal(http.ListenAndServe(ip, nil))
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	host, port := "localhost", "8080"
+
+	switch len(os.Args) {
+	case 3:
+		port = os.Args[2]
+		host = os.Args[1]
+	case 2:
+		host = os.Args[1]
+	}
+
+	ipAddress := host + ":" + port
+
+	http.HandleFunc("/", handler)
+
+	go serve(ipAddress)
+
+	fmt.Println("Server listening on " + ipAddress)
+	fmt.Scanln()
 }
